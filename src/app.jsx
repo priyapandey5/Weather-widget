b78be807c898682961dbbe7237ff3d6708c81356
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2024-08-03 10:35:35 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,16 @@
 
+import Button from '@mui/material/Button';
+//import SearchBox from './SearchBox';
+//import InfoBox from './InfoBox';
+import WeatherApp from "./WeatherApp";
+
+function App() {
+  return (
+    <>
+      <WeatherApp/>
+      
+    </>
+  )
+}
+
+export default App;
